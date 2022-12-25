7a28f8115fde9ac11e15fc3fb523b2241d370e24
OwO Swordsman <owoswordsman@gmail.com>
2022-12-24 19:53:08 -0500

    Collect accelerometer data

--- main.py
+++ main.py
@@ -4,16 +4,27 @@ from adafruit_circuitplayground.express import cpx as cp
 
 
 def main() -> None:
-    cp.pixels.brightness = 0.1
+    # cp.pixels.brightness = 0.1
+    print("running...")
+    start = time.monotonic()
 
-    while True:
-        for i in range(len(cp.pixels)):
-            cp.pixels[i] = (15 + i * 10, 15, 15)
-            time.sleep(0.15)
+    try:
+        with open("data.csv", "w") as file:
+            file.write("time,x,y,z\n")
+            while True:
+                # for i in range(len(cp.pixels)):
+                #     cp.pixels[i] = (15 + i * 10, 15, 15)
+                #     time.sleep(0.15)
 
-        for i in range(len(cp.pixels)):
-            cp.pixels[i] = (0, 0, 0)
-            time.sleep(0.15)
+                # for i in range(len(cp.pixels)):
+                #     cp.pixels[i] = (0, 0, 0)
+                #     time.sleep(0.15)
+                x, y, z = cp.acceleration
+                # print("{},{},{},{}".format(time.monotonic() - start, x, y, z))
+                file.write("{},{},{},{}\n".format(time.monotonic() - start, x, y, z))
+                time.sleep(0.1)
+    except OSError:
+        print("read-only fs")
 
 
 if __name__ == "__main__":

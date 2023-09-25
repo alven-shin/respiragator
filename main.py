6060ced6d0a63e9ee2024b854921d89816e3091f
Alvin Shen <29553973+asian-dev@users.noreply.github.com>
2023-09-25 15:00:13 -0400

    Remove comments

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# import time
-
 import board
 import busio
 
-# from adafruit_circuitplayground.express import cpx as cp
 from analogio import AnalogIn
 
 SAMPLE_SIZE = 100
 
 
 def main() -> None:
-    # cp.pixels.brightness = 0.1
     uart = busio.UART(board.TX, board.RX, baudrate=9600)
     analog_in = AnalogIn(board.A1)
 
@@ -27,34 +23,6 @@ def main() -> None:
             uart.write(int.to_bytes(int(avg / 10), 1, "big"))
         except OverflowError:
             print(avg / 10)
-
-
-"""
-    start = time.monotonic()
-    try:
-        with open("data.csv", "w") as file:
-            # file.write("time,data\n")
-            while True:
-                cp.pixels.fill((0, 50, 0))
-                sum = 0
-                for _ in range(SAMPLE_SIZE):
-                    sum += analog_in.value
-
-                # print("{},{},{},{}".format(time.monotonic() - start, x, y, z))
-                file.write(
-                    "{},{}\n".format(time.monotonic() - start, sum / SAMPLE_SIZE)
-                )
-                if time.monotonic() > 15:
-                    break
-        while True:
-            cp.pixels.fill((50, 50, 50))
-    except OSError:
-        print("read-only fs")
-        while True:
-            cp.pixels.fill((50, 0, 00))
-    # sum = 0
-    # for _ in range(1000):
-"""
 
 
 if __name__ == "__main__":
